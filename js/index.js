ba3b87e918755a6903cd764d8d67f66941e40ac8
istiaq-ahsan <ahsanistiaq23@gmail.com>
2024-10-07 00:17:18 +0600

    50 marks task complete

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ const loadALLCards = async () => {
     displayAllPets(data.pets);
 }
 
-//-------------------------------------------3rd
+//-------------------------------------------for show catagory wise pet
 //create loadCatagoryCard
 
 const loadCategoryCards = async (categoryWise) => {
@@ -24,6 +24,8 @@ const loadCategoryCards = async (categoryWise) => {
     displayAllPets(data.data);
 }
 
+//call from card and then go to create modal
+
 const modalPetDetails = async (petId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${petId}`)
     const data = await response.json();
@@ -31,7 +33,7 @@ const modalPetDetails = async (petId) => {
 }
 
 
-//create displayAllPets
+//create displayAllPets-------------------------------------from 2nd global
 
 const displayAllPets = (pets) => {
     const allPetsContainer = document.getElementById("allPetsContainer");
@@ -40,10 +42,10 @@ const displayAllPets = (pets) => {
     if (pets.length == 0) {
         allPetsContainer.classList.remove("grid")
         allPetsContainer.innerHTML = `
-        <div class="min-h-[360px] w-[850px] flex flex-col gap-5 text-center items-center justify-center space-y-3 py-24 bg-gray-200 rounded-xl">
+        <div class="min-h-[360px] lg:w-[850px] flex flex-col gap-5 text-center items-center justify-center space-y-3 py-24 bg-gray-200 rounded-xl">
         <img src="./images/error.webp" alt="" />
         <h1 class="text-2xl font-bold">No Information Available</h1>
-        <p class="w-[600px] mx-auto">It is a long established fact that a reader will be distracted by the readable content of a page when looking at 
+        <p class="lg:w-[600px] mx-auto">It is a long established fact that a reader will be distracted by the readable content of a page when looking at 
 its layout. The point of using Lorem Ipsum is that it has a.</p>
         </div>
         `;
@@ -56,24 +58,25 @@ its layout. The point of using Lorem Ipsum is that it has a.</p>
     pets.forEach((pet) => {
         console.log(pet);
 
-        //create card
+        //create card-------------
+
         const card = document.createElement("div");
         card.classList = "card border"
         card.innerHTML = `
         <figure class="px-5 pt-5">
-                            <img src=${pet.image}
+                            <img class="w-full object-cover rounded-xl" src=${pet.image}
                                 alt="" class="rounded-xl" />
                         </figure>
                         <div class="card-body">
                             <h2 class="text-xl font-bold">${pet.pet_name}</h2>
-                            <p><i class="fa-solid fa-border-all"></i> ${pet.breed}</p>
-                            <p><i class="fa-regular fa-calendar"></i> ${pet.date_of_birth}</p>
-                            <p><i class="fa-solid fa-mercury"></i> ${pet.gender}</p>
-                            <p><i class="fa-solid fa-dollar-sign"></i> ${pet.price}</p>
+                            <p><i class="fa-solid fa-border-all"></i> Breed: ${pet.breed}</p>
+                            <p><i class="fa-regular fa-calendar"></i> Birth: ${pet.date_of_birth}</p>
+                            <p><i class="fa-solid fa-mercury"></i> Gender: ${pet.gender}</p>
+                            <p><i class="fa-solid fa-dollar-sign"></i> Price: ${pet.price}</p>
                             
                             <hr />
 
-                            <div class="flex justify-around text-center">
+                            <div class="flex justify-around text-center gap-2">
                                 <button id="addToList" onclick="shiftRight('${pet.image}')" class="btn bg-white border-gray-200"><i
                                         class="fa-regular fa-thumbs-up"></i></button>
                                 <button class="btn bg-white border-gray-200">Adopt</button>
@@ -86,7 +89,7 @@ its layout. The point of using Lorem Ipsum is that it has a.</p>
     })
 }
 
-//rightSideShift
+//right Side Shift --------------------------------call from card for show right side
 
 const shiftRight = (petImage) => {
     const shiftRightContainer = document.getElementById("shiftRightContainer");
@@ -100,7 +103,7 @@ const shiftRight = (petImage) => {
 }
 
 
-//---------------------------------------2nd
+//---------------------------------------from 1st global 
 //create displayCatagories
 
 const displayCategories = (categories) => {
@@ -113,8 +116,8 @@ const displayCategories = (categories) => {
         const buttonContainer = document.createElement("div");
 
         buttonContainer.innerHTML = `
-        <button onclick="loadCategoryCards('${item.category.toLowerCase()}')" class="btn bg-white border-gray-300 lg:w-[260px] lg:h-[70px]">
-        <img src=${item.category_icon} alt="" /> ${item.category}
+        <button onclick="loadCategoryCards('${item.category.toLowerCase()}')" class="btn bg-white border-gray-300 w-[150px] h-[50px] lg:w-[260px] lg:h-[70px]">
+        <img class="w-10 h-10" src=${item.category_icon} alt="" /> ${item.category}
         </button>
         `
 
@@ -123,20 +126,23 @@ const displayCategories = (categories) => {
     });
 }
 
-//petDetails
+//petDetails modal----------------------------------again call from card details
 
 const petDetails = (details) => {
 
     const { breed, gender, vaccinated_status, date_of_birth, price, image, pet_name, pet_details } = details
 
     const modalContainer = document.getElementById("modal-container");
+
+    modalContainer.innerHTML = "";
+
     const div = document.createElement("div");
     div.innerHTML = `
     <dialog id="my_modal_1" class="modal">
         <div class="modal-box">
             
         <div>
-            <img class="object-cover w-full" src=${image} alt="" />
+            <img class="object-cover w-full rounded-xl" src=${image} alt="" />
         </div>
         <div class="my-5">
             <h2 class="text-xl font-bold">${pet_name}</h2>
